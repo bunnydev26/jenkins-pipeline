bd162545404b5962e927ad32276409a4f0b770fb
bunnydev26 <58064446+bunnydev26@users.noreply.github.com>
2019-11-23 01:53:19 +0530

    Update aka.groovy

--- vars/aka.groovy
+++ vars/aka.groovy
@@ -1,9 +1,10 @@
-def call(application_name, module_name) {
+def call(gitUsername, repositoryName) {
 
     stage('checkout') {
         node('master') {
             // Clear all the files and directory for the clean build
-            sh "echo Hello world";
+            bunnydev26/jenkins-pipeline
+            sh "git clone --single-branch -b ${repositoryName}  https://github.com/${gitUsername}/${repositoryName}.git"
         }
     }
 }
