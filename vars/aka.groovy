e34932e0d949ffcdcf5fb1214ae52cb3b318bbd4
bunnydev26 <58064446+bunnydev26@users.noreply.github.com>
2019-11-23 18:40:41 +0530

    Update aka.groovy

--- vars/aka.groovy
+++ vars/aka.groovy
@@ -15,10 +15,10 @@ def call(gitUsername, repositoryName, dockerUsername) {
             imageVersion = date.format("yyddMMHHmm");
             // Docker image version
             dockerImageName = "${dockerUsername}/${repositoryName}:${imageVersion}";
-            sh "docker build -t ${dockerImageName} ./${repositoryName}";
+            
         }
     }
-    /*
+    
     stage('Build and Test') {
         node('master') {
             // Building the docker image.
@@ -56,5 +56,5 @@ def call(gitUsername, repositoryName, dockerUsername) {
             sh "echo 'Deploy on Production'";
             sh "python3 ~/kube_renderer/kube_renderer.py --instance_status production --docker_image_version ${imageVersion} --base_dir ./${repositoryName} | kubectl apply -f -"
         }
-    }*/
+    }
 }
