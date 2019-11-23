8a387e95fe3aa889ab9cc23c00fc97e532f781d5
bunnydev26 <58064446+bunnydev26@users.noreply.github.com>
2019-11-23 14:43:54 +0530

    Update aka.groovy

--- vars/aka.groovy
+++ vars/aka.groovy
@@ -3,7 +3,7 @@ def call(gitUsername, repositoryName) {
     stage('checkout') {
         node('master') {
             // Clear all the files and directory for the clean build
-            // yes
+            // yes re
             sh "git clone --single-branch -b master https://github.com/${gitUsername}/${repositoryName}.git"
         }
     }
