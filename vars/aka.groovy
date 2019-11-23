6142fcda1f7d34179e12228ab08b826ce078dcad
bunnydev26 <58064446+bunnydev26@users.noreply.github.com>
2019-11-23 16:55:28 +0530

    Update aka.groovy

--- vars/aka.groovy
+++ vars/aka.groovy
@@ -37,7 +37,7 @@ def call(gitUsername, repositoryName, dockerUsername) {
         }
     }
     
-    stage('Confirm to Deploy O production') {
+    stage('Confirm') {
         def userInput = input(
             id: 'userInput', message: 'This is PRODUCTION!', parameters: [
             [$class: 'BooleanParameterDefinition', defaultValue: false, description: '', name: 'Please confirm you sure to proceed']
