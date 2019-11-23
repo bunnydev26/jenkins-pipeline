257483cb4241ee2620e9a7cea98e56a0c61786f6
bunnydev26 <58064446+bunnydev26@users.noreply.github.com>
2019-11-23 16:45:23 +0530

    Update aka.groovy

--- vars/aka.groovy
+++ vars/aka.groovy
@@ -1,10 +1,30 @@
-def call(gitUsername, repositoryName) {
+def call(gitUsername, repositoryName, dockerUsername) {
 
     stage('checkout') {
         node('master') {
             // Clear all the files and directory for the clean build
             sh "rm -rf *";
+            // Cloning the repostiroy which stores the data to build the docker file and the kube deployment files
             sh "git clone --single-branch -b master https://github.com/${gitUsername}/${repositoryName}.git";
+            // Pulling the changes to the renderer repository.
+            sh "cd ~/kube_renderer && git pull";
+            
+            // Populating the image version.
+            imageVersion = date.format("yyddMMHHmm");
+        }
+    }
+    stage('Build and Test') {
+        node('master') {
+            // Building the docker image.
+            dockerImageName = "${dockerUsername}/${repositoryName}:${imageVersion}";
+            sh "echo 'docker build -t ${dockerImageName} .'";
+            
+            // Runing the test cases on the docker image if it is applicable.
+            sh "echo 'Running test cases'"
+            
+            // Pushing the docker image.
+            sh "echo 'docker push ${dockerImageName}'"
+            
         }
     }
 }
